12915ec273a4346de38d0c147709445c9d5eaecc
Yunong Huang <yunong.huang@microfocus.com>
2021-05-29 10:00:59 +0800

    add color sharder

---
+++ OpenGL/glfw_add_color.cpp
@@ -0,0 +1,228 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+/*
+在OpenGL中，任何事物都在3D空间中，而屏幕和窗口却是2D像素数组，这导致OpenGL的大部分工作都是关于把3D坐标转变为适应你屏幕的2D像素。
+3D坐标转为2D坐标的处理过程是由OpenGL的图形渲染管线（Graphics Pipeline，大多译为管线，实际上指的是一堆原始图形数据途经一个输送管道，
+期间经过各种变化处理最终出现在屏幕的过程）管理的。图形渲染管线可以被划分为两个主要部分：第一部分把你的3D坐标转换为2D坐标，
+第二部分是把2D坐标转变为实际的有颜色的像素。这个教程里，我们会简单地讨论一下图形渲染管线，以及如何利用它创建一些漂亮的像素。
+
+
+图形渲染管线接受一组3D坐标，然后把它们转变为你屏幕上的有色2D像素输出。图形渲染管线可以被划分为几个阶段，每个阶段将会把前一个阶段的输出作为输入。
+所有这些阶段都是高度专门化的（它们都有一个特定的函数），并且很容易并行执行。正是由于它们具有并行执行的特性，当今大多数显卡都有成千上万的小处理核心，
+它们在GPU上为每一个（渲染管线）阶段运行各自的小程序，从而在图形渲染管线中快速处理你的数据。这些小程序叫做着色器(Shader)。
+
+有些着色器允许开发者自己配置，这就允许我们用自己写的着色器来替换默认的。这样我们就可以更细致地控制图形渲染管线中的特定部分了，
+而且因为它们运行在GPU上，所以它们可以给我们节约宝贵的CPU时间。OpenGL着色器是用OpenGL着色器语言(OpenGL Shading Language, GLSL)写成的
+
+link: https://learnopengl-cn.github.io/img/01/04/pipeline.png
+*/
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// 
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+//const GLchar* vertexShaderSource = "#version 330 core\n"
+//"layout (location = 0) in vec3 position;\n"
+//"layout (location = 1) in vec3 color;\n"
+//"out vec3 ourColor;\n"
+//"void main()\n"
+//"{\n"
+//"gl_Position = vec4(position, 1.0);\n"
+//"ourColor = color;\n"
+//"}\0";
+//const GLchar* fragmentShaderSource = "#version 330 core\n"
+//"out vec4 color;\n"
+//"uniform vec4 ourColor;\n"
+//"void main()\n"
+//"{\n"
+//"color = ourColor;\n"
+//"}\n\0";
+
+// 非unifrom设置颜色
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"layout (location = 1) in vec3 color;\n"
+"out vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position, 1.0);\n"
+"ourColor = color;\n"
+"}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"in vec3 ourColor; \n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(ourColor, 1.0f);\n"
+"}\n\0";
+
+int main()
+{
+	//调用glfwInit函数来初始化GLFW
+	glfwInit();
+
+	//使用glfwWindowHint函数来配置GLFW。glfwWindowHint函数的第一个参数代表选项的名称，
+	//我们可以从很多以GLFW_开头的枚举值中选择；第二个参数接受一个整型，用来设置这个选项的值
+	//教程都是基于OpenGL 3.3版本展开讨论的，所以我们需要告诉GLFW我们要使用的OpenGL版本是3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//同样明确告诉GLFW我们使用的是核心模式(Core-profile)
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//创建窗口对象
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	//glfwMakeContextCurrent(window);
+
+	//指定窗口的OpenGL或OpenGL ES上下文在调用线程上处于当前状态。一个上下文一次只能在一个线程上是当前的，每个线程一次只能有一个当前上下文
+	glfwMakeContextCurrent(window);
+	//设置指定窗口的framebuffer resize回调函数，当指定窗口的framebuffer重新调整大小时调用该回调函数。
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	//GLAD传入了用来加载系统相关的OpenGL函数指针地址的函数。GLFW给我们的是glfwGetProcAddress，它根据我们编译的系统定义了正确的函数
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// 渲染一个三角形
+	// 编译着色器
+	// 创建定着色器
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// 着色器源码与对象绑定
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	// 编译着色器
+	glCompileShader(vertexShader);
+	// 校验着色器是否编译失败
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// 片段着色器
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//着色器程序对象(Shader Program Object)是多个着色器合并之后并最终链接完成的版本。如果要使用刚才编译的着色器我们必须把它们链接(Link)为一个着色器程序对象，
+	//然后在渲染对象的时候激活这个着色器程序。已激活着色器程序的着色器将在我们发送渲染调用的时候被使用。
+	unsigned int shaderProgram = glCreateProgram();
+	//绑定到shaderProgram对象
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	//校验link是否有问题
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	//在把着色器对象链接到程序对象以后，记得删除着色器对象，我们不再需要它们了 个人觉得可以理解为移动所有权
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// 设置三个定点数据
+	//float vertices[] = {
+	//	-0.5f, -0.5f, 0.0f, // left  
+	//	 0.5f, -0.5f, 0.0f, // right 
+	//	 0.0f,  0.5f, 0.0f  // top   
+	//};
+	//设置定点数据的颜色
+	GLfloat vertices[] = {
+		// 位置              // 颜色
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+	};
+
+
+	//首先绑定顶点数组对象，然后绑定和设置顶点缓冲区，然后配置顶点属性
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+
+	//复制顶点数组到缓冲中供OpenGL使用
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//设置顶点属性
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	//glEnableVertexAttribArray(0);
+
+	// 位置属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// 颜色属性
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// 程序在我们主动关闭它之前不断绘制图像并能够接受用户输入
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		// -----
+		processInput(window);
+
+		// render
+		// ------
+		//glClearColor来设置清空屏幕所用的颜色
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		//当调用glClear函数，清除颜色缓冲之后，整个颜色缓冲都会被填充为glClearColor里所设置的颜色
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// 当我们渲染一个物体时要使用着色器程序
+		glUseProgram(shaderProgram);
+
+		// Update the uniform color
+		/*GLfloat timeValue = glfwGetTime();
+		GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
+		GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.2f, greenValue, 0.0f, 1.0f);*/
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// glfwSwapBuffers函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上 -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		// glfwPollEvents函数检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glfwTerminate();
+
+	return 0;
+
+}
